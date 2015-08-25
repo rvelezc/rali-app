3d3f5e94930433761391956b1af9552a2642c90c
Rafael Velez <rafael.velez.c@gmail.com>
2015-08-25 11:10:25 -0500

    Update create.sql

--- install/database/create.sql
+++ install/database/create.sql
@@ -3,11 +3,11 @@ drop database IF EXISTS rali_marketing;
 create database rali_marketing;
 -- drop user rali;
 -- create user rali;
-grant usage on *.* to rali@'%' identified by 'passw0rd';
-grant file on *.* to rali@'%' identified by 'passw0rd';
+grant usage on *.* to rali@'%' identified by '';
+grant file on *.* to rali@'%' identified by '';
 grant all privileges on rali_marketing.* to rali@'%';
-grant usage on *.* to rali@'localhost' identified by 'passw0rd';
-grant file on *.* to rali@'localhost' identified by 'passw0rd';
+grant usage on *.* to rali@'localhost' identified by '';
+grant file on *.* to rali@'localhost' identified by '';
 grant all privileges on rali_marketing.* to rali@'localhost';
 FLUSH PRIVILEGES;
 
